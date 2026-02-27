9663898c15470fb04f1079c20131213b60d11dad
David Stockdale <davidstockdale13@hotmail.co.uk>
2026-02-27 15:25:40 +0000

    Cleanup
    Formatted some messy code and preloaded some images.

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 require_once('config.php');
 global $api_key2;
 global $api_key3;
@@ -18,124 +18,139 @@ global $api_key6;
 global $api_key7;
 global $api_key10;
 
-$container = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod('understrap_container_type');
 ?>
 
-<?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
+<?php get_template_part('sidebar-templates/sidebar', 'footerfull'); ?>
 
 <div class="wrapper" id="wrapper-footer">
-	
 
-	<div class="<?php echo esc_attr( $container ); ?>">
+
+	<div class="<?php echo esc_attr($container); ?>">
 
 		<div class="row">
 
-				<footer class="site-footer" id="colophon">
-					
-<!-- 	Preload Font Awsome -->
-					<script defer src="/wp-content/themes/underdavid-rcvda/fonts/fontawesome-free-6.7.2-web/js/all.js">						</script>
-					
-					<!-- Global site tag (gtag.js) - Google Analytics -->
-					<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $api_key2; ?>"></script>
-					<script>
-					  window.dataLayer = window.dataLayer || [];
-					  function gtag(){dataLayer.push(arguments);}
-						gtag('js', new Date());
-						gtag('config', '<?php echo $api_key2; ?>');
-						gtag('config', '<?php echo $api_key3; ?>');
-						gtag('config', '<?php echo $api_key4; ?>');
-					</script>
-					
-					<!-- Google Tag Manager -->
-					<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-					})(window,document,'script','dataLayer','<?php echo $api_key5; ?>');</script>
-					<!-- End Google Tag Manager -->
+			<footer class="site-footer" id="colophon">
 
-					<!-- Event snippet for Page view conversion page -->
-					<script>
-					  gtag('event', 'conversion', {'send_to': '<?php echo $api_key6; ?>'});
-					</script>
-					
-					<!-- Attempt to clear Mailchimp Session (only works when navigating between posts/pages) -->
-					<script>
-// 						function cleanup() {
-						var cookies = document.cookie.split("; ");
-							// Loop through all cookies
-							for (var i = 0; i < cookies.length; i++) {
-								var cookie = cookies[i];
+				<!-- 	Preload Font Awsome -->
+				<script defer
+					src="/wp-content/themes/underdavid-rcvda/fonts/fontawesome-free-6.7.2-web/js/all.js"></script>
 
-								// Split the cookie name and value
-								var eqPos = cookie.indexOf("=");
-								var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+				<!-- Global site tag (gtag.js) - Google Analytics -->
+				<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $api_key2; ?>"></script>
+				<script>
+					window.dataLayer = window.dataLayer || [];
+					function gtag() { dataLayer.push(arguments); }
+					gtag('js', new Date());
+					gtag('config', '<?php echo $api_key2; ?>');
+					gtag('config', '<?php echo $api_key3; ?>');
+					gtag('config', '<?php echo $api_key4; ?>');
+				</script>
 
-								// Check if cookie name matches the mcforms-*sessionId pattern
-								if (/^mcforms-\d+-sessionId$/.test(name)) {
-									// Remove the cookie by setting it to expire in the past
-									document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-								}
-									document.cookie = "_mcid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-									document.cookie = "_abck=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-									document.cookie = "bm_sv=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-							}
-// 						}
-					</script>
-					
-					
-<!-- 					Mailchimp -->
-					<script async id="mcjs1">
-						function showPopup() { 
-							!function(c,h,i,m,p){
-								m=c.createElement(h),
-									p=c.getElementsByTagName(h)[0],
-									m.async=1,
-									m.src=i,
-									p.parentNode.insertBefore(m,p)
-								}(
-									document,
-									"script",
-									"https://chimpstatic.com/mcjs-connected/js/users/<?php echo $api_key7; ?>.js"
-								);
+				<!-- Google Tag Manager -->
+				<script>(function (w, d, s, l, i) {
+						w[l] = w[l] || []; w[l].push({
+							'gtm.start':
+								new Date().getTime(), event: 'gtm.js'
+						}); var f = d.getElementsByTagName(s)[0],
+							j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+								'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+					})(window, document, 'script', 'dataLayer', '<?php echo $api_key5; ?>');</script>
+				<!-- End Google Tag Manager -->
+
+				<!-- Event snippet for Page view conversion page -->
+				<script>
+					gtag('event', 'conversion', { 'send_to': '<?php echo $api_key6; ?>' });
+				</script>
+
+				<!-- Attempt to clear Mailchimp Session (only works when navigating between posts/pages) -->
+				<script>
+					// 						function cleanup() {
+					var cookies = document.cookie.split("; ");
+					// Loop through all cookies
+					for (var i = 0; i < cookies.length; i++) {
+						var cookie = cookies[i];
+
+						// Split the cookie name and value
+						var eqPos = cookie.indexOf("=");
+						var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+
+						// Check if cookie name matches the mcforms-*sessionId pattern
+						if (/^mcforms-\d+-sessionId$/.test(name)) {
+							// Remove the cookie by setting it to expire in the past
+							document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 						}
+						document.cookie = "_mcid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+						document.cookie = "_abck=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+						document.cookie = "bm_sv=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+					}
+					// 						}
+				</script>
 
-						document.getElementById("show-popup").onclick = function() {showPopup(); }
 
-						function showPopup2() { 
-							!function(c,h,i,m,p){
-								m=c.createElement(h),
-									p=c.getElementsByTagName(h)[0],
-									m.async=1,
-									m.src=i,
-									p.parentNode.insertBefore(m,p)
-								}(
-									document,
-									"script",
-									"https://chimpstatic.com/mcjs-connected/js/users/<?php echo $api_key10; ?>.js"
-								);
-						}
+				<!-- 					Mailchimp -->
+				<script async id="mcjs1">
+					function showPopup() {
+						!function (c, h, i, m, p) {
+							m = c.createElement(h),
+								p = c.getElementsByTagName(h)[0],
+								m.async = 1,
+								m.src = i,
+								p.parentNode.insertBefore(m, p)
+						}(
+							document,
+							"script",
+							"https://chimpstatic.com/mcjs-connected/js/users/<?php echo $api_key7; ?>.js"
+						);
+					}
 
-						document.getElementById("show-popup2").onclick = function() {showPopup2();}
-					</script>
-<!-- 				End Mailchimp -->
-					
-					<?php
-					/**
-					 * Adds the "logo-strip" widget area!!!
-					 */
-					if ( !function_exists( 'dynamic_sidebar' ) 
-						|| !dynamic_sidebar('logo-strip') )
-					?>
-					
-					<div class="site-info">
-						
-						<?php understrap_site_info(); ?>
-						
-					</div><!-- .site-info -->
+					document.getElementById("show-popup").onclick = function () { showPopup(); }
 
-				</footer><!-- #colophon -->
-			
+					function showPopup2() {
+						!function (c, h, i, m, p) {
+							m = c.createElement(h),
+								p = c.getElementsByTagName(h)[0],
+								m.async = 1,
+								m.src = i,
+								p.parentNode.insertBefore(m, p)
+						}(
+							document,
+							"script",
+							"https://chimpstatic.com/mcjs-connected/js/users/<?php echo $api_key10; ?>.js"
+						);
+					}
+
+					document.getElementById("show-popup2").onclick = function () { showPopup2(); }
+				</script>
+				<!-- 				End Mailchimp -->
+
+				<link rel="preload" as="image" media=”all”
+					href="/wp-content/uploads/2026/02/Disability-Confident-Employer-Logo-1.webp" async>
+				<link rel="preload" as="image" media=”all”
+					href="/wp-content/uploads/2026/02/Race-At-Work-Charter-Signatory-Logo-1.webp" async>
+				<link rel="preload" as="image" media=”all”
+					href="/wp-content/uploads/2026/02/Mindful-Employer-Logo-1.webp" async>
+				<link rel="preload" as="image" media=”all”
+					href="/wp-content/uploads/2026/02/Armed-Forces-Covenant-Logo-1.webp" async>
+
+				<?php
+				/**
+				 * Adds the "logo-strip" widget area!!!
+				 */
+				if (
+					!function_exists('dynamic_sidebar')
+					|| !dynamic_sidebar('logo-strip')
+				)
+				?>
+
+				<div class="site-info">
+
+					<?php understrap_site_info(); ?>
+
+				</div><!-- .site-info -->
+
+			</footer><!-- #colophon -->
+
 
 
 		</div><!-- row end -->
@@ -151,4 +166,3 @@ $container = get_theme_mod( 'understrap_container_type' );
 </body>
 
 </html>
-
